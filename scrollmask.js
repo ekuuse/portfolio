b95c4489deb011ff2ed98a5c76deef2ac60499a2
ekuuse <erki.kuuse@voco.ee>
2026-01-09 10:25:28 +0200

    minor style changes

--- scrollmask.js
+++ scrollmask.js
@@ -2,9 +2,8 @@ const mask = document.getElementById("particle-mask");
 
 window.addEventListener("scroll", () => {
     const scrollTop = window.scrollY;
-    const docHeight = document.body.scrollHeight - window.innerHeight;
 
-    const progress = Math.min(scrollTop / docHeight, 1);
+    const progress = Math.min(scrollTop / 1080, 1);
 
     // move gradient up as you scroll
     mask.style.transform = `translateY(${100 - progress * 100}%)`;
